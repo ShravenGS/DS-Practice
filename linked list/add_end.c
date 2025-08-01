57b29ee1e6aa4971fb19cacaaf846e4282908c9d
Shraven GS <gsshraven@gmail.com>
2025-08-01 09:23:57 +0530

    Create add_end.c

---
+++ linked list/add_end.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int data;
+	struct node *next;
+};
+
+struct node *head=NULL;
+void add_end(){
+        struct node *temp,*newNode;
+        int data;
+        newNode=(struct node*)malloc(sizeof(struct node));
+        if(newNode==NULL){
+                printf("Memory not allocated");
+                return;
+        }
+        printf("Enter the data");
+        scanf("%d",&data);
+        newNode->data=data;
+        newNode->next=NULL;
+
+        if(head==NULL){
+                head=newNode;
+        }
+
+        else{
+                temp=head;
+
+                while(temp->next!=NULL){
+                        temp=temp->next;
+                }
+                temp->next=newNode;
+        }
+}
+
+
+void display(){
+	struct node *temp=head;
+	if(head==NULL){
+		printf("List is empty");
+	}
+	else{
+		while(temp!=NULL){
+			printf("%d ",temp->data);
+			temp=temp->next;
+		}
+	}
+}
+
+int main(){
+	int choice;
+	do{
+		add_end();
+		
+		printf("Do you want to add more:");
+		scanf("%d",&choice);
+	}while(choice==1);
+	display();
+}
